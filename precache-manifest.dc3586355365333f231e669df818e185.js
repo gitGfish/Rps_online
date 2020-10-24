674793b23c31e1b5acb7efe47a2f6f7c7e642692
gitGfish <49610748+gitGfish@users.noreply.github.com>
2020-10-24 21:52:30 +0300

    Updates

---
+++ precache-manifest.dc3586355365333f231e669df818e185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e9ea28dfd4fb156c525737f19673aa",
+    "url": "/Rps_online/index.html"
+  },
+  {
+    "revision": "1d6163f68022de9b04a6",
+    "url": "/Rps_online/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5163a082eed10601fef2",
+    "url": "/Rps_online/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d6163f68022de9b04a6",
+    "url": "/Rps_online/static/js/2.92accfc0.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/Rps_online/static/js/2.92accfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5163a082eed10601fef2",
+    "url": "/Rps_online/static/js/main.6796d2fb.chunk.js"
+  },
+  {
+    "revision": "11317c9f71e7bd06a215",
+    "url": "/Rps_online/static/js/runtime-main.3a5696d8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rps_online/new_client/precache-manifest.5b546eaa9adc9c5009d4485ea5fb240c.js"
+  "/Rps_online/precache-manifest.dc3586355365333f231e669df818e185.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rps_online/new_client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rps_online/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
