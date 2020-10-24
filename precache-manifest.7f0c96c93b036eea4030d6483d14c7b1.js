e600a933965587a9e918cd933bfff4618199395b
gitGfish <49610748+gitGfish@users.noreply.github.com>
2020-10-24 21:09:38 +0300

    Updates

---
+++ precache-manifest.7f0c96c93b036eea4030d6483d14c7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ec9edaecea20b68c51f2a101e4487a",
+    "url": "/Rps_online/index.html"
+  },
+  {
+    "revision": "1d6163f68022de9b04a6",
+    "url": "/Rps_online/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5257484e62ac2a862484",
+    "url": "/Rps_online/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d6163f68022de9b04a6",
+    "url": "/Rps_online/static/js/2.92accfc0.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/Rps_online/static/js/2.92accfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5257484e62ac2a862484",
+    "url": "/Rps_online/static/js/main.4beb2111.chunk.js"
+  },
+  {
+    "revision": "11317c9f71e7bd06a215",
+    "url": "/Rps_online/static/js/runtime-main.3a5696d8.js"
+  }
+]);
